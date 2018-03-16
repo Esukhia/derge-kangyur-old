7c0708b9e7fe77432f269c3d095261c3cedb8c29
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-03-16 12:31:46 +0100

    add tei export script (WIP but almost done)

---
+++ scripts/toTei.py
@@ -0,0 +1,89 @@
+import re
+
+
+PAREN_RE = re.compile(r"\(([^\),]*),([^\),]*)\)")
+
+def parse_one_line(line, filelinenum, state, outf, options):
+    if filelinenum == 1:
+        outf.write("title: "+line)
+        return
+    pagelinenum = ''
+    endpnumi = line.find(']')
+    if endpnumi == -1:
+        print("error on line "+str(filelinenum)+" cannot find ]")
+        return
+    pagelinenum = line[1:endpnumi]
+    pagenum = -1
+    pageside = -1
+    linenum = 0
+    doti = pagelinenum.find('.')
+    if doti == -1:
+        try:
+            pagenum = int(pagelinenum[:-1])
+        except ValueError:
+            print("error on line "+str(filelinenum)+" cannot convert page to integer")
+            return
+        pageside = pagelinenum[-1]
+        if pageside not in ['a', 'b']:
+            print("error on line "+str(filelinenum)+" cannot understand page side")
+            return
+    else:
+        pagenumstr = pagelinenum[0:doti-1]
+        linenumstr = pagelinenum[doti+1:]
+        try: 
+            pagenum = int(pagenumstr)
+            linenum = int(linenumstr)
+        except ValueError:
+            print("error on line "+str(filelinenum)+" cannot convert page / line to integer")
+            return
+        pageside = pagelinenum[doti-1]
+        if pageside not in ['a', 'b']:
+            print("error on line "+str(filelinenum)+" cannot understand page side")
+            return
+    newpage = False
+    if 'pagenum' in state and 'pageside' in state:
+        oldpagenum = state['pagenum']
+        oldpageside = state['pageside']
+        if oldpagenum != pagenum and oldpagenum != pagenum-1:
+            print("error on line "+str(filelinenum)+" leap in page numbers from "+str(oldpagenum)+" to "+str(pagenum))
+        if oldpagenum == pagenum and oldpageside == 'b' and pageside == 'a':
+            print("error on line "+str(filelinenum)+" going backward in page sides")
+        if oldpagenum == pagenum-1 and (pageside == 'b' or oldpageside == 'a'):
+            print("error on line "+str(filelinenum)+" leap in page sides")
+        if oldpagenum != pagenum or oldpageside != pageside:
+            newpage = True
+    state['pagenum'] = pagenum
+    state['pageside'] = pageside
+    if 'linenum' in state and linenum != 0:
+        oldlinenum = state['linenum']
+        if oldlinenum != linenum and oldlinenum != linenum-1:
+            print("error on line "+str(filelinenum)+" leap in line numbers from "+str(oldlinenum)+" to "+str(linenum))
+    state['linenum'] = linenum
+    if len(line) > endpnumi+1:
+        text = line[endpnumi+1:]
+        if 'keep_errors_indications' not in options or not options['keep_errors_indications']:
+            text = text.replace('[', '').replace(']', '')
+        if 'fix_errors' not in options or not options['fix_errors']:
+            text = re.sub(r"\(([^\),]*),([^\),]*)\)", r"\1", text)
+        else:
+            text = re.sub(r"\(([^\),]*),([^\),]*)\)", r"\1", text)
+    if newpage:
+        outf.write('</tei:p><tei:p n="'+str(pagenum)+pageside+'">')
+    outf.write('<tei:milestone unit="line" n="'+str(linenum)+'"/>'+text)
+
+def parse_one_file(infilename, outfilename, options):
+    with open(infilename, 'r', encoding="utf-16le") as inf:
+        with open(outfilename, 'w', encoding="utf-8") as outf:
+            state = {}
+            linenum = 1
+            for line in inf:
+                parse_one_line(line, linenum, state, outf, options)
+                linenum += 1
+
+if __name__ == '__main__':
+    """ Example use """
+    options = {
+        "fix_errors": False,
+        "keep_errors_indications": False
+    }
+    parse_one_file('../derge-kangyur-tags/080 FINAL tags.txt', '../output/test.xml', options)
