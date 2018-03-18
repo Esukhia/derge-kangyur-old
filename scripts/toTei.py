43dd955bdeb4c3559825b4953063155cbcaadab5
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-03-18 10:04:06 +0100

    progress on TEI export

--- scripts/toTei.py
+++ scripts/toTei.py
@@ -1,6 +1,39 @@
 import re
 import os
 
+TEI_BEGINNING = """
+<?xml version="1.0" encoding="UTF-8"?>
+<tei:TEI xmlns:tei="http://www.tei-c.org/ns/1.0">
+  <tei:teiHeader>
+    <tei:fileDesc>
+      <tei:titleStmt>
+        <tei:title>{title} [{volnum}]</tei:title>
+      </tei:titleStmt>
+      <tei:publicationStmt>
+        <tei:distributor>Text input by Esukhia for 84000, with support from BDRC and THDL at UVa. This TEI files has been automatically produced by a script from raw text files available on https://github.com/Esukhia/derge-kangyur</tei:distributor>
+        <tei:idno type="TBRC_TEXT_RID">UT4CZ5369-I1KG9{ignum}-0000</tei:idno>
+        <tei:idno type="page_equals_image">page_equals_image</tei:idno>
+      </tei:publicationStmt>
+      <tei:sourceDesc>
+        <tei:bibl>
+          <tei:idno type="TBRC_RID">W4CZ5369</tei:idno>
+          <tei:idno type="SRC_PATH">eKangyur/W4CZ5369/sources/W4CZ5369-I1KG9{ignum}/W4CZ5369-I1KG9{ignum}-0000.txt</tei:idno>
+        </tei:bibl>
+      </tei:sourceDesc>
+    </tei:fileDesc>
+  </tei:teiHeader>
+  <tei:text>
+    <tei:body>
+      <tei:div>
+        <tei:p n="1">{title}</tei:p>
+"""
+
+TEI_END = """
+      </tei:div>
+    </tei:body>
+  </tei:text>
+</tei:TEI>
+"""
 
 PAREN_RE = re.compile(r"\(([^\),]*),([^\),]*)\)")
 
@@ -15,9 +48,11 @@ def parrepl(match, mode, filelinenum):
         print("error on line "+str(filelinenum)+" tsheg not matching in parenthesis")
     return mode == 'first' and first or sec
 
-def parse_one_line(line, filelinenum, state, outf, options):
+def parse_one_line(line, filelinenum, state, outf, volnum, options):
     if filelinenum == 1:
-        outf.write("title: "+line)
+        ignum = volnum + 126
+        header = TEI_BEGINNING.format(title = line, volnum = volnum, ignum = ignum)
+        outf.write(header)
         return
     pagelinenum = ''
     endpnumi = line.find(']')
@@ -71,6 +106,7 @@ def parse_one_line(line, filelinenum, state, outf, options):
         if oldlinenum != linenum and oldlinenum != linenum-1:
             print("error on line "+str(filelinenum)+" leap in line numbers from "+str(oldlinenum)+" to "+str(linenum))
     state['linenum'] = linenum
+    text = ''
     if len(line) > endpnumi+1:
         text = line[endpnumi+1:]
         if 'keep_errors_indications' not in options or not options['keep_errors_indications']:
@@ -82,17 +118,20 @@ def parse_one_line(line, filelinenum, state, outf, options):
         if text.find('(') != -1 or text.find(')') != -1:
             print("error on line "+str(filelinenum)+", spurious parenthesis")
     if newpage:
-        outf.write('</tei:p><tei:p n="'+str(pagenum)+pageside+'">')
-    outf.write('<tei:milestone unit="line" n="'+str(linenum)+'"/>'+text)
+        outf.write('</tei:p>\n        <tei:p n="'+str(pagenum)+pageside+'">')
+    if text != '':
+        outf.write('<tei:milestone unit="line" n="'+str(linenum)+'"/>'+text)
 
-def parse_one_file(infilename, outfilename, options):
+def parse_one_file(infilename, outfilename, volnum, options):
     with open(infilename, 'r', encoding="utf-16le") as inf:
         with open(outfilename, 'w', encoding="utf-8") as outf:
             state = {}
             linenum = 1
             for line in inf:
-                parse_one_line(line, linenum, state, outf, options)
+                # [:-1] to remove final line break
+                parse_one_line(line[:-1], linenum, state, outf, volnum, options)
                 linenum += 1
+            outf.write(TEI_END)
 
 if __name__ == '__main__':
     """ Example use """
@@ -100,11 +139,9 @@ if __name__ == '__main__':
         "fix_errors": False,
         "keep_errors_indications": False
     }
-    for i in range(1, 102):
-        num = '{0:03d}'.format(i)
-        infilename = '../derge-kangyur-tags/'+num+' FINAL tags.txt'
+    for volnum in range(1, 103):
+        volnumstr = '{0:03d}'.format(volnum)
+        infilename = '../derge-kangyur-tags/'+volnumstr+'-tagged.txt'
         print("transforming "+infilename)
         os.makedirs('./output/', exist_ok=True)
-        parse_one_file(infilename, './output/'+num+'.xml', options)
-    # text = "ན་ཏན་(བརྫོད་‚བརྗོད)པར་མཛད་པར་བཞེད"
-    # print(re.sub(r"\(([^\),]*),([^\),]*)\)", r"\2", text))
+        parse_one_file(infilename, './output/W4CZ5369-I1KG9'+str(volnum+126)+'-0000.xml', volnum, options)
