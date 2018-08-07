11a25186e4731c73d613948ffb1b4bb68b05986d
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-08-07 09:20:04 +0200

    add other editions

--- scripts/showimage.js
+++ scripts/showimage.js
@@ -21,22 +21,36 @@ function pad(n, width, z) {
 }
 
 function getVolInfo(volStr) {
-	volId = 'bdr:V22084_I0'.concat(parseInt(volStr)+885);
-	return volId;
+	volId22 = 'bdr:V22084_I0'.concat(parseInt(volStr)+885);
+	volId4C = 'bdr:V4CZ5369_I1KG9'.concat(parseInt(volStr)+126);
+	volId30 = 'bdr:V30532_I6'.concat(parseInt(volStr)+347);
+	return [volId4C, volId22, volId30];
 }
 
 function getImageName(volume, imageNum) {
-	var volNum = parseInt(volume)+885;
+	res = [];
+	var volInt = parseInt(volume);
 	var paddedImageNum = pad(''+imageNum, 4, '0');
-	return '0'+volNum+paddedImageNum+'.tif';
+	var	volNum = volInt+126;
+	res[0] = 'I1KG9'+volNum+paddedImageNum+'.jpg';
+	volNum = volInt+885;
+	res[1] = '0'+volNum+paddedImageNum+'.tif';
+	volNum = volInt+347;
+	res[2] = '6'+volNum+paddedImageNum+'.tif';
+	return res;
 }
 
 function printimage() {
 	var volume = document.getElementById('volume').value;
 	var page = document.getElementById('page').value;
 	var imageNum = getPageInfo(page);
-	var volId = getVolInfo(volume);
-	var imageName = getImageName(volume, imageNum);
-	var imageUrl = 'http://iiif.bdrc.io/image/v2/'+volId+'::'+imageName+'/full/full/0/default.jpg';
-	document.getElementById('theimage').src = imageUrl;
+	if (imageNum == null) {
+		return;
+	}
+	var volIds = getVolInfo(volume);
+	var imageNames = getImageName(volume, imageNum);
+	for (var i = 0 ; i < 3 ; i++) {
+		var imageUrl = 'http://iiif.bdrc.io/image/v2/'+volIds[i]+'::'+imageNames[i]+'/full/full/0/default.jpg';
+		document.getElementById('theimage'+(i+1)).src = imageUrl;
+	}
 }
